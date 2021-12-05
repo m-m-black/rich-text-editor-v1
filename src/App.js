645db456efb7b1baf3e5fc18fa1aa8739615af15
m-m-black <morganblack1988@gmail.com>
2021-12-05 12:07:38 +1100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-function App() {
+const App = () => {
+  const baseURL =
+    'https://ipchgmr3va.execute-api.ap-southeast-2.amazonaws.com/prod';
+
+  const [fileData, setFileData] = useState('');
+
+  const handleOnChange = (e, editor) => {
+    setFileData(editor.getData());
+  };
+
+  const createFile = () => {
+    axios
+      .post(`${baseURL}/?name=newfile`, {
+        content: fileData,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.appContainer}>
+      <div style={styles.buttonContainer}>
+        <button onClick={createFile}>Create file</button>
+      </div>
+      <div style={styles.mainContainer}>
+        <div style={styles.editorContainer}>
+          <CKEditor editor={ClassicEditor} onChange={handleOnChange} />
+        </div>
+        <div style={styles.displayContainer}>{fileData}</div>
+      </div>
     </div>
   );
-}
+};
+
+const styles = {
+  appContainer: {
+    margin: 20,
+    height: '100vh',
+  },
+  buttonContainer: {
+    marginBottom: 20,
+  },
+  mainContainer: {
+    display: 'flex',
+  },
+  editorContainer: {
+    marginRight: 10,
+    width: '50%',
+    height: '100%',
+  },
+  displayContainer: {
+    marginLeft: 10,
+    width: '50%',
+  },
+};
 
 export default App;
